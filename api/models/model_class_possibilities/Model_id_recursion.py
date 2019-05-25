c4e65a61b74a41374fc9abf5dec643cd44f125bd
Yoann Guillard <yoann9344@gmail.com>
2019-05-25 11:09:18 +0200

    Create Model_id_recursion.py (#22)
    * Create Model_id_recursion.py
    
    I have achieved the perfect recursion without using depth. We should using it at the creation of the database : 
    db = SQLAlchemy(app, model_class=MyModel)
    Cause with we use a mixin and we forgot to use, the if an a class with mixin has a relationship with one without mixin it will create a complicate bug.
    And this because we do this test : if issubclass(type(attribute), MyModel)
    so if MyModel is a mixin, we might skip the recursive and send back a dict with an object inside !
    
    * Update Model_id_recursion.py
    
    I added static types
    
    * Update Model_id_recursion.py

---
+++ api/models/model_class_possibilities/Model_id_recursion.py
@@ -0,0 +1,61 @@
+# for more detail, see class Model in :
+# https://github.com/pallets/flask-sqlalchemy/blob/master/flask_sqlalchemy/model.py
+from typing import Any, List, Type
+
+
+class ModelSQL(object):
+    query_class = None  # flask_alchemy attribute
+    query = None  # flask_alchemy attribute
+
+    DONOTSEND_MODEL = {'_sa_instance_state'}
+    DONOTSEND = []
+
+    def __repr__(self) -> str:
+        return '<{}>'.format(self.__class__.__name__)
+
+    def to_dict_recursive(self) -> dict:
+        return self._to_dict_recursive(obj_ids_crossed=[id(self)])
+
+    def _to_dict_recursive(self, obj_ids_crossed: List[int]) -> dict:
+        """ iterate through objects to create a dict
+        
+        Keywords arguments :
+        obj_ids_crossed -- List of objects' id already passed through, provides against circular recursion
+        
+        Inside functions :
+        check_crossed_obj -- Check if object has already been passed through
+        type_shunt_recursive -- Select actions for each type of attr
+        """
+        def check_crossed_obj(obj: Type[ModelSQL]) -> any:
+            if id(obj) in obj_ids_crossed:
+                return str(obj)
+                # others possibilities
+                # return str(obj).join(' ').join(str(obj.id))
+                # return obj.id
+            else:
+                obj_ids_crossed.append(id(obj))
+                return obj._to_dict_recursive(obj_ids_crossed)
+
+        def type_shunt_recursive(attribute: Any) -> Any:
+            # model
+            if issubclass(type(attribute), ModelSQL):
+                return check_crossed_obj(attribute)
+            # recursive iteration of the list in case of the list is a relationship
+            elif isinstance(attribute, list):
+                values = []
+                for item in attribute:
+                    values.append(type_shunt_recursive(item))
+                return values
+            # attribute is not an instance of relationship (int, str..)
+            else:
+                return attribute
+
+        result = {}
+        # __mapper__ is equivalent to db.inspect(self)
+        # but db (database) is not created yet cause we send this model to the constructor
+        for key in self.__mapper__.attrs.keys():
+            if key not in self.DONOTSEND:
+                attr = getattr(self, key)
+                result[key] = type_shunt_recursive(attr)
+
+return result
